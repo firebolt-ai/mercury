bce4e530f477270ca0d4843ac4ea179bb30090cd
Aaron Ma <aaronhma@gmail.com>
2020-08-18 13:16:46 -0700

    Start Mercury base

---
+++ src/plugins/system/name_plugin.h
@@ -0,0 +1,16 @@
+#ifndef MERCURY_NAME_PLUGIN_
+#define MERCURY_NAME_PLUGIN_
+
+#include <bits/stdc++.h>
+
+std::string setSystemName(bool enableBeastMode /*, MercurySystem system*/)
+{
+  if (enableBeastMode)
+  {
+    // system._setBeastMode();
+  }
+
+  return std::string("Mercury");
+}
+
+#endif // MERCURY_NAME_PLUGIN_
